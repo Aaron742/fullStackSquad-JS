310ac693a122ea9becfd68bc1572561dce26a169
Aaron742 <103736717+Aaron742@users.noreply.github.com>
2026-05-12 11:24:18 +0200

    ajout date et objet pizza dans exo Strings

--- fullStack-JS/src/lessons/exoStrings/strings.js
+++ fullStack-JS/src/lessons/exoStrings/strings.js
@@ -2,17 +2,20 @@ let date = new Date();
 let day = date.getDate();
 let month = date.getMonth() + 1;
 let year = date.getFullYear();
-let prix = 10.5;
+let prix = 13.5;
 let nom = "Ketchum";
 let prenom = "Ash";
-let pizza = "pizza bolognaise";
+let pizza = {
+    nom: "pizza bolognaise",
+    supplements: "ananas, kebab, fromage"
+};
 let adresse = "Bourg Palette, Kanto";
 let merci = 'Merci d\'avoir commandé chez "La Pizzeria Raffinata" !';
 let blague = `---------------------------------- BLaGuE ----------------------------------
 Qu'est-ce qu'une pizza a dit à une autre pizza qui lui demandait des conseils ? "Suis ta pâte et tout ira bien !"`;
 
 const SumUpOrderPhrase = `Le ${day}-${month}-${year} :
-Bonjour/Bonsoir ${prenom} ${nom} votre ${pizza} (${prix}₽) est bien prête, elle vous sera livrée dans 30 minutes à l'adresse suivante : ${adresse}.
+Bonjour/Bonsoir ${prenom} ${nom} votre ${pizza.nom} avec suppléments ${pizza.supplements} (${prix}₽) est bien prête, elle vous sera livrée dans 30 minutes à l'adresse suivante : ${adresse}.
 ${merci}
 ${blague}`;
 
