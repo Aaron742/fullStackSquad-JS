19b8db2ff1c85ce5671daac980a67654f0d08125
Aaron742 <103736717+Aaron742@users.noreply.github.com>
2026-05-12 10:27:32 +0200

    history.html et exo strings ajoutés

--- fullStack-JS/src/lessons/exoStrings/strings.js
+++ fullStack-JS/src/lessons/exoStrings/strings.js
@@ -7,8 +7,6 @@ let merci = 'Merci d\'avoir commandé chez "Raffinata" !'
 let blague = `----------------- BLaGuE -----------------
 Qu'est qu'une pizza a dit à une autre pizza qui lui demandait des conseils ? "Suis ta pâte et tout ira bien ! LOL"`;
 
-console.log(
-  `Bonjour/Bonsoir ${prenom} ${nom} votre ${pizza} est bien prête, elle vous sera livrée dans 30 minutes à l'adresse suivante : ${adresse}.
+const SumUpOrderPhrase =  `Bonjour/Bonsoir ${prenom} ${nom} votre ${pizza} est bien prête, elle vous sera livrée dans 30 minutes à l'adresse suivante : ${adresse}.
   ${merci}
   ${blague}`
-);
