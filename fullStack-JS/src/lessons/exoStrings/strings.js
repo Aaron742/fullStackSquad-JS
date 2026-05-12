1d07f3f80f5d2ad006a96de44d9ef166bb2c700a
Aaron742 <103736717+Aaron742@users.noreply.github.com>
2026-05-12 11:18:01 +0200

    ajout date dans exo Strings

--- fullStack-JS/src/lessons/exoStrings/strings.js
+++ fullStack-JS/src/lessons/exoStrings/strings.js
@@ -1,16 +1,19 @@
-let date = "12/05/2026 :"
-let prix = 10.5
-let nom = 'Ketchum'
+let date = new Date();
+let day = date.getDate();
+let month = date.getMonth() + 1;
+let year = date.getFullYear();
+let prix = 10.5;
+let nom = "Ketchum";
 let prenom = "Ash";
-let pizza = 'pizza bolognaise'
-let adresse = 'Bourg Palette, Kanto'
-let merci = 'Merci d\'avoir commandé chez "La Pizzeria Raffinata" !'
+let pizza = "pizza bolognaise";
+let adresse = "Bourg Palette, Kanto";
+let merci = 'Merci d\'avoir commandé chez "La Pizzeria Raffinata" !';
 let blague = `---------------------------------- BLaGuE ----------------------------------
 Qu'est-ce qu'une pizza a dit à une autre pizza qui lui demandait des conseils ? "Suis ta pâte et tout ira bien !"`;
 
-const SumUpOrderPhrase = `Le ${date}
+const SumUpOrderPhrase = `Le ${day}-${month}-${year} :
 Bonjour/Bonsoir ${prenom} ${nom} votre ${pizza} (${prix}₽) est bien prête, elle vous sera livrée dans 30 minutes à l'adresse suivante : ${adresse}.
 ${merci}
 ${blague}`;
 
-console.log(SumUpOrderPhrase)
+console.log(SumUpOrderPhrase);
