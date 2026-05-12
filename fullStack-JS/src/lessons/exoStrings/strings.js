d15194773dd0b92aa50c9d685a52632020ffecb5
Aaron742 <103736717+Aaron742@users.noreply.github.com>
2026-05-12 11:11:06 +0200

    history.html et exo strings ajoutés

--- fullStack-JS/src/lessons/exoStrings/strings.js
+++ fullStack-JS/src/lessons/exoStrings/strings.js
@@ -1,11 +1,16 @@
-let nom = 'Dupont'
-let prenom = "Jean";
+let date = "12/05/2026 :"
+let prix = 10.5
+let nom = 'Ketchum'
+let prenom = "Ash";
 let pizza = 'pizza bolognaise'
-let adresse = 'Toulouse'
-let merci = 'Merci d\'avoir commandé chez "Raffinata" !'
-let blague = `----------------- BLaGuE -----------------
-Qu'est qu'une pizza a dit à une autre pizza qui lui demandait des conseils ? "Suis ta pâte et tout ira bien ! LOL"`;
+let adresse = 'Bourg Palette, Kanto'
+let merci = 'Merci d\'avoir commandé chez "La Pizzeria Raffinata" !'
+let blague = `---------------------------------- BLaGuE ----------------------------------
+Qu'est-ce qu'une pizza a dit à une autre pizza qui lui demandait des conseils ? "Suis ta pâte et tout ira bien !"`;
 
-const SumUpOrderPhrase =  `Bonjour/Bonsoir ${prenom} ${nom} votre ${pizza} est bien prête, elle vous sera livrée dans 30 minutes à l'adresse suivante : ${adresse}.
-  ${merci}
-  ${blague}`
+const SumUpOrderPhrase = `Le ${date}
+Bonjour/Bonsoir ${prenom} ${nom} votre ${pizza} (${prix}₽) est bien prête, elle vous sera livrée dans 30 minutes à l'adresse suivante : ${adresse}.
+${merci}
+${blague}`;
+
+console.log(SumUpOrderPhrase)
