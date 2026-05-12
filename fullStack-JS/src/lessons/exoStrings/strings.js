2927703c8c347c0f1fd665dcb3329f083443b59a
Aaron742 <103736717+Aaron742@users.noreply.github.com>
2026-05-12 10:24:30 +0200

    history.html et exo strings ajoutés

---
+++ fullStack-JS/src/lessons/exoStrings/strings.js
@@ -0,0 +1,14 @@
+let prix = 15
+let nom = 'Dupont'
+let prenom = "Jean";
+let pizza = 'pizza bolognaise'
+let adresse = 'Toulouse'
+let merci = 'Merci d\'avoir commandé chez "Raffinata" !'
+let blague = `----------------- BLaGuE -----------------
+Qu'est qu'une pizza a dit à une autre pizza qui lui demandait des conseils ? "Suis ta pâte et tout ira bien ! LOL"`;
+
+console.log(
+  `Bonjour/Bonsoir ${prenom} ${nom} votre ${pizza} est bien prête, elle vous sera livrée dans 30 minutes à l'adresse suivante : ${adresse}.
+  ${merci}
+  ${blague}`
+);
