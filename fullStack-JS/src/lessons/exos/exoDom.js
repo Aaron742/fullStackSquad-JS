2fdb63131c3d9e134a227bb38a23617300f10971
Aaron742 <103736717+Aaron742@users.noreply.github.com>
2026-05-19 13:31:11 +0200

    exo dom

--- fullStack-JS/src/lessons/exos/exoDom.js
+++ fullStack-JS/src/lessons/exos/exoDom.js
@@ -15,6 +15,8 @@ let firstImg = document.createElement('img')
 
 firstImg.src = "https://www.boredpanda.com/blog/wp-content/uploads/2022/06/funny-low-cost-cosplay-pics-62a744d39c80a__700.jpg";
 
+firstImg.alt = 'Photo magnifique'
+
 myDiv.append(firstImg);
 
 let name = document.createElement('h1')
@@ -44,10 +46,17 @@ myDiv.append(dob);
 let active = document.createElement("p");
 
 // CONDITION TERNAIRE
-active.innerText = userData.active = true ? 'connecté' : 'déconnecté';
+active.innerText = userData.active === true ? 'online' : 'offline';
 
 myDiv.append(active);
 
-myDiv.style.backgroundColor = 'royalblue';
+myDiv.style.backgroundImage = "linear-gradient(45deg, rgb(65, 89, 208) 0%, rgb(200, 79, 192) 50%, rgb(255, 205, 112) 100%)";
 myDiv.style.color = "white";
 myDiv.style.fontWeight = 'bold'
+myDiv.style.width = "15%"
+myDiv.style.padding = "2%"
+myDiv.style.margin = 'auto'
+myDiv.style.border = "1px solid black";
+myDiv.style.boxShadow = "-5px 5px 10px black";
+myDiv.style.textShadow = "2px 2px 10px black";
+firstImg.style.width = "100%"
