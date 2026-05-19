ec85fefcc224bb5bce1db95ce91437d73b86579f
Aaron742 <103736717+Aaron742@users.noreply.github.com>
2026-05-19 11:10:52 +0200

    exo dom

--- fullStack-JS/src/lessons/exos/exoDom.js
+++ fullStack-JS/src/lessons/exos/exoDom.js
@@ -1,7 +1,51 @@
-let lesTxt = document.getElementsByTagName('p')
-console.log(lesTxt)
+//! EXO 20.1
+//TODO: via JS afficher le profil  utilisateur dans la page web
+const userData = {
+    name: "John delavega",
+    email: "john.doe@example.com",
+    age: 25,
+    dob: "08/02/1989",
+    active: true,
+    img: "https://www.boredpanda.com/blog/wp-content/uploads/2022/06/funny-low-cost-cosplay-pics-62a744d39c80a__700.jpg",
+};
 
-let txtTab = Array.from(lesTxt)
-console.log(txtTab)
+const myDiv = document.querySelector('.userProfile')
 
-txtTab.map((x) => {x.innerText = 'Salut'})
+let firstImg = document.createElement('img')
+
+firstImg.src = "https://www.boredpanda.com/blog/wp-content/uploads/2022/06/funny-low-cost-cosplay-pics-62a744d39c80a__700.jpg";
+
+myDiv.append(firstImg);
+
+let name = document.createElement('h1')
+
+name.innerText = userData.name
+
+myDiv.append(name)
+
+let email = document.createElement("p");
+
+email.innerText = userData.email;
+
+myDiv.append(email);
+
+let age = document.createElement("p");
+
+age.innerText = userData.age;
+
+myDiv.append(age);
+
+let dob = document.createElement("p");
+
+dob.innerText = userData.dob;
+
+myDiv.append(dob);
+
+let status = document.createElement("p");
+
+status.innerText = userData.active;
+
+myDiv.append(status);
+
+myDiv.style.backgroundColor = 'royalblue';
+myDiv.style.color = "white";
