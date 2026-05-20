c43e58050177f2e48c478bc0a98e9f311f544811
Aaron742 <103736717+Aaron742@users.noreply.github.com>
2026-05-20 11:01:57 +0200

    exo dom classList

---
+++ fullStack-JS/src/lessons/exos/exoDOM3.js
@@ -0,0 +1,28 @@
+const title = document.querySelector("h1");
+
+// Ajouter
+
+const btnOne = document.querySelector("#ajouter");
+console.log(btnOne);
+
+btnOne.addEventListener("click", () => {
+  title.classList.add("bg-primary");
+});
+
+// Supprimer
+
+const btnTwo = document.querySelector("#supprimer");
+console.log(btnTwo);
+
+btnTwo.addEventListener("click", () => {
+  title.classList.remove("bg-primary");
+});
+
+// Toggle
+
+const btnThree = document.querySelector("#activer");
+console.log(btnThree);
+
+btnThree.addEventListener("click", () => {
+  title.classList.toggle("bg-primary");
+});
