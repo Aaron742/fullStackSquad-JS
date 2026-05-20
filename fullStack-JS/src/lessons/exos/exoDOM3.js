7cea7e688cdab3ca505197ca5e7a48930fe60454
Aaron742 <103736717+Aaron742@users.noreply.github.com>
2026-05-20 14:54:12 +0200

    exo dom event focus & blur

--- fullStack-JS/src/lessons/exos/exoDOM3.js
+++ fullStack-JS/src/lessons/exos/exoDOM3.js
@@ -18,30 +18,30 @@ btnTwo.addEventListener("click", () => {
 
 // Toggle
 
-const btnThree = document.querySelector("#activer");
+// const btnThree = document.querySelector("#activer");
 
-btnThree.addEventListener("click", () => {
-    title.classList.toggle("bg-primary");
-});
+// btnThree.addEventListener("click", () => {
+//     title.classList.toggle("bg-primary");
+// });
 
 // Exo image on click
 
-document.addEventListener('click', (clickEvent) => {
-    // console.log("coord X", clickEvent.pageX);
-    // console.log("coord Y", clickEvent.pageY);
-    // console.log("coord X", clickEvent.x);
-    // console.log("coord Y", clickEvent.y);
-    console.log(clickEvent)
-    let addImg = document.createElement("img");
-    const imageSize = 500
-    addImg.src = `https://picsum.photos/${imageSize}/${imageSize}`;
-    addImg.style.position = "absolute";
-    addImg.style.border = '3px solid black'
-    addImg.style.boxShadow = '5px 5px 500px black'
-    addImg.style.top = clickEvent.y - imageSize/2 + 'px'
-    addImg.style.left = clickEvent.x - imageSize/2 + 'px'
-    document.body.append(addImg)
-})
+// document.addEventListener('click', (clickEvent) => {
+//     // console.log("coord X", clickEvent.pageX);
+//     // console.log("coord Y", clickEvent.pageY);
+//     // console.log("coord X", clickEvent.x);
+//     // console.log("coord Y", clickEvent.y);
+//     console.log(clickEvent)
+//     let addImg = document.createElement("img");
+//     const imageSize = 500
+//     addImg.src = `https://picsum.photos/${imageSize}/${imageSize}`;
+//     addImg.style.position = "absolute";
+//     addImg.style.border = '3px solid black'
+//     addImg.style.boxShadow = '5px 5px 500px black'
+//     addImg.style.top = clickEvent.y - imageSize/2 + 'px'
+//     addImg.style.left = clickEvent.x - imageSize/2 + 'px'
+//     document.body.append(addImg)
+// })
 
 // let btnCompteur = document.querySelector('.counter')
 
@@ -50,3 +50,16 @@ document.addEventListener('click', (clickEvent) => {
 //     comp
 //     btnCompteur.innerHTML = `<span>${compteur}</span>`
 // })
+
+// exo focus & blur
+
+let focusBlur = document.querySelector('input')
+
+focusBlur.addEventListener('focus', () => {
+    focusBlur.style.backgroundColor = 'royalblue'
+})
+
+focusBlur.addEventListener('blur', () => {
+    focusBlur.style.backgroundColor = 'transparent'
+    focusBlur.style.color = 'black'
+})
